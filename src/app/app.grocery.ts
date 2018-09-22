31e47fa22aeb232ddab8a914901246a0be88827f
Esther <esther.youri.shin@gmail.com>
2018-09-22 14:09:01 -0700

    implemented strike through and css

--- src/app/app.grocery.ts
+++ src/app/app.grocery.ts
@@ -19,7 +19,9 @@ export class GroceryComponent {
 
     onClick = () => {
         if (this.task.id == 0) {
-            this.tasks.push({ id: (new Date()).getTime(), name: this.task.name });
+            this.tasks.push({ id: (new Date()).getTime(), name: this.task.name, strike: false });
+        
+        
             this.task = {
                 name: '',
                 id: 0
@@ -48,4 +50,22 @@ export class GroceryComponent {
             }
         }
     }
+
+    onStrike(item){
+    for(var i=0;i<this.tasks.length; i++){
+        if(item.id==this.tasks[i].id){
+            if(this.tasks[i].strike){
+                this.tasks[i].strike = false;
+            }
+
+            else{
+                this.tasks[i].strike = true;
+            }
+            break;
+        }
+    }
+    }
+
+
 }
+
